ccfb462b6493e330cb82a0df9e452df64489bbae
Ditmar Wendt <doubledhelix@gmail.com>
2011-11-14 17:32:54 -0500

    commented, finalized, sped up a bit

--- goggleabuserlinux/gogglesabuser.cpp
+++ goggleabuserlinux/gogglesabuser.cpp
@@ -1,4 +1,3 @@
-
 #include <curl/curl.h>
 #include <stdlib.h>
 #include <sstream>
@@ -31,36 +30,42 @@ int main( int argc, char* argv[] )
 	BMP toPost;
 	toPost.ReadFromFile("post.bmp");
 	
-//	pointData dotArray[toPost.TellWidth()][toPost.TellHeight()];
-	
 	int scale = 1;
 	int xoffset = 0;
 	int yoffset = 0;
 	int diameter = 7;
 	
+	int width = toPost.TellWidth();
+	int height = toPost.TellHeight();
 	
-	cout << "Width: " << toPost.TellWidth() << " Height: " << toPost.TellHeight() << " Pixels: " << toPost.TellWidth()*toPost.TellHeight() << endl;
+	cout << "Width: " << width << " Height: " << height << " Pixels: " << width*height << endl;
 
 
 	CURL *curl;
 	CURLcode res;
 	
 	curl = curl_easy_init();
+	//you can probably mess with some other options to increase speed, but
+	//afaik it's limited by the server's actual response time(the goggles server)
+	//so it's destined to be sort of slow forever(their sqldb is huge and thus slow)
+	//maybe: figure out how to do asynchronous calls instead of the blocking curl_easy_perform
+	curl_easy_setopt(curl, CURLOPT_MAXCONNECTS, 1000);
+	
 	if(curl) {
 		
-		for(int x=0; x<toPost.TellWidth(); x++) {
-			for(int y=0; y<toPost.TellHeight(); y++) {
+		for(int x=0; x<width; x++) {
+			for(int y=0; y<height; y++) {
 				
 				string pointSerialized = numToB64(x * scale + xoffset) + numToB64(y * scale + yoffset);
-				
-	//			cout << pointSerialized << endl;
-	//			cout << (int)toPost(x,y)->Red << endl;
+
+				cout << x << "/" << width << " " << y << "/" << height << " ";
 				
 				//yes this is bad coding
-				string myURL = "http%3A%2F%2Fwww.mspaintadventures.com%2F%2F";
-				string fullURL = "http://goggles.sneakygcr.net/page?callback=fuckoff&page=" + myURL + "&add=t&title=GNAA&r=" + to_string((int)toPost(x,y)->Red) + "&g=" +  to_string((int)toPost(x,y)->Green) + "&b=" + to_string((int)toPost(x,y)->Blue) + "&a=1&t=" + to_string(diameter) + "&p=" + pointSerialized + "&_=1420891562000";
+				string goatmanTarget = "http%3A%2F%2Fwww.mspaintadventures.com%2F%2F";
+				string fullURL = "http://goggles.sneakygcr.net/page?callback=fuckoff&page=" + goatmanTarget + "&add=t&title=GNAA&r=" + to_string((int)toPost(x,y)->Red) + "&g=" +  to_string((int)toPost(x,y)->Green) + "&b=" + to_string((int)toPost(x,y)->Blue) + "&a=1&t=" + to_string(diameter) + "&p=" + pointSerialized + "&_=1420891562000";
 				
-	//			system(("curl \"" + fullURL + "\"").c_str());
+				
+//				system(("curl \"" + fullURL + "\"").c_str());
 				res = curl_easy_setopt(curl, CURLOPT_URL, fullURL.c_str());
 				
 				curl_easy_perform(curl);
@@ -69,6 +74,8 @@ int main( int argc, char* argv[] )
 			}
 		}
 	
+	} else {
+		cout << "curl failed to init" << endl;
 	}
 
 	return 0;
